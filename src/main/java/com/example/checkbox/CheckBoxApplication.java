74bdcff52e36adf3d83bf692c8e8bcaaabc64134
Daniel <dmarrom577@g.educaand.es>
2022-09-26 17:33:27 +0200

    Version de Casa

---
+++ src/main/java/com/example/checkbox/CheckBoxApplication.java
@@ -0,0 +1,55 @@
+package com.example.checkbox;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CheckBoxApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Scene scene = new Scene(grid, 300, 275);
+        stage.setScene(scene);
+        stage.setTitle("CheckBox Wiki");
+        stage.show();
+
+        //Titulo EjemploCheckBox
+        Text scenetitle = new Text("Ejemplo de CheckBox");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        //CheckBox sin titulo
+        CheckBox cb1 = new CheckBox();
+        grid.add(cb1, 0, 1);
+
+        //Asignacion de titulo a CheckBox 1
+        cb1.setText("Casado");
+
+        //Marcar por defecto CheckBox 1
+        cb1.setSelected(true);
+
+        //CheckBox con titulo
+        CheckBox cb2 = new CheckBox("Soltero");
+        grid.add(cb2, 0, 2);
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
--- src/main/java/com/example/checkbox/CheckBoxController.java
+++ src/main/java/com/example/checkbox/CheckBoxController.java
@@ -3,7 +3,7 @@ package com.example.checkbox;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class CheckBoxController {
     @FXML
     private Label welcomeText;
 
